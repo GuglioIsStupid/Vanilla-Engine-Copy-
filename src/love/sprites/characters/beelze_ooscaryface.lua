dbf692f00762502d699d12527c332e2f4a9a44ae
GuglioIsStupid <bradsbluetab@gmail.com>
2022-10-24 16:21:54 -0400

    dds files oo spooky

---
+++ src/love/sprites/characters/beelze_ooscaryface.lua
@@ -0,0 +1,44 @@
+return graphics.newSprite(
+	love.graphics.newImage(graphics.imagePath("path/to/image")), -- Do not add the .png extension
+	{
+		{x = 3, y = 3, width = 325, height = 527, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 1: OldMan_Creepy_Down0000
+		{x = 3, y = 3, width = 325, height = 527, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 2: OldMan_Creepy_Down0001
+		{x = 331, y = 3, width = 325, height = 527, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 3: OldMan_Creepy_Down0002
+		{x = 331, y = 3, width = 325, height = 527, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 4: OldMan_Creepy_Down0003
+		{x = 659, y = 3, width = 325, height = 527, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 5: OldMan_Creepy_Down0004
+		{x = 659, y = 3, width = 325, height = 527, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 6: OldMan_Creepy_Down0005
+		{x = 987, y = 3, width = 256, height = 546, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 7: OldMan_Creepy_Idle0000
+		{x = 987, y = 3, width = 256, height = 546, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 8: OldMan_Creepy_Idle0001
+		{x = 1246, y = 3, width = 256, height = 546, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 9: OldMan_Creepy_Idle0002
+		{x = 1246, y = 3, width = 256, height = 546, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 10: OldMan_Creepy_Idle0003
+		{x = 1505, y = 3, width = 256, height = 546, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 11: OldMan_Creepy_Idle0004
+		{x = 1505, y = 3, width = 256, height = 546, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 12: OldMan_Creepy_Idle0005
+		{x = 3, y = 552, width = 256, height = 546, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 13: OldMan_Creepy_Idle0006
+		{x = 3, y = 552, width = 256, height = 546, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 14: OldMan_Creepy_Idle0007
+		{x = 262, y = 552, width = 256, height = 546, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 15: OldMan_Creepy_Idle0008
+		{x = 262, y = 552, width = 256, height = 546, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 16: OldMan_Creepy_Idle0009
+		{x = 521, y = 552, width = 393, height = 538, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 17: OldMan_Creepy_Left0000
+		{x = 521, y = 552, width = 393, height = 538, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 18: OldMan_Creepy_Left0001
+		{x = 917, y = 552, width = 393, height = 538, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 19: OldMan_Creepy_Left0002
+		{x = 917, y = 552, width = 393, height = 538, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 20: OldMan_Creepy_Left0003
+		{x = 1313, y = 552, width = 393, height = 538, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 21: OldMan_Creepy_Left0004
+		{x = 1313, y = 552, width = 393, height = 538, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 22: OldMan_Creepy_Left0005
+		{x = 3, y = 1101, width = 238, height = 540, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 23: OldMan_Creepy_Right0000
+		{x = 3, y = 1101, width = 238, height = 540, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 24: OldMan_Creepy_Right0001
+		{x = 244, y = 1101, width = 238, height = 540, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 25: OldMan_Creepy_Right0002
+		{x = 244, y = 1101, width = 238, height = 540, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 26: OldMan_Creepy_Right0003
+		{x = 485, y = 1101, width = 238, height = 540, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 27: OldMan_Creepy_Right0004
+		{x = 485, y = 1101, width = 238, height = 540, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 28: OldMan_Creepy_Right0005
+		{x = 726, y = 1101, width = 322, height = 556, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 29: OldMan_Creepy_Up0000
+		{x = 726, y = 1101, width = 322, height = 556, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 30: OldMan_Creepy_Up0001
+		{x = 1051, y = 1101, width = 322, height = 556, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 31: OldMan_Creepy_Up0002
+		{x = 1051, y = 1101, width = 322, height = 556, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 32: OldMan_Creepy_Up0003
+		{x = 1376, y = 1101, width = 322, height = 556, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 33: OldMan_Creepy_Up0004
+		{x = 1376, y = 1101, width = 322, height = 556, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0} -- 34: OldMan_Creepy_Up0005
+	},
+	{
+		["anim"] = {start = 1, stop = 1, speed = 0, offsetX = 0, offsetY = 0},
+	},
+	"anim", -- set to default animation
+	false -- If the sprite repeats
+)
