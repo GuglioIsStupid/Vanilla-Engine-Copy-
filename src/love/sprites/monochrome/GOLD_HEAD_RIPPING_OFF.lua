c8efe4c58670fedb5d11df12109fbbed784b9c93
clothing-hanger <kelbymixon1@gmail.com>
2022-10-24 21:05:43 -0500

    the funny files

---
+++ src/love/sprites/monochrome/GOLD_HEAD_RIPPING_OFF.lua
@@ -0,0 +1,80 @@
+return graphics.newSprite(
+	love.graphics.newImage(graphics.imagePath("monochrome/GOLD_HEAD_RIPPING_OFF")), -- Do not add the .png extension
+	{
+		{x = 5, y = 5, width = 391, height = 579, offsetX = -65, offsetY = -107, offsetWidth = 547, offsetHeight = 847}, -- 1: Head rips_OneLayer instance 10000
+		{x = 5, y = 5, width = 391, height = 579, offsetX = -65, offsetY = -107, offsetWidth = 547, offsetHeight = 847}, -- 2: Head rips_OneLayer instance 10001
+		{x = 396, y = 5, width = 391, height = 583, offsetX = -65, offsetY = -106, offsetWidth = 547, offsetHeight = 847}, -- 3: Head rips_OneLayer instance 10002
+		{x = 396, y = 5, width = 391, height = 583, offsetX = -65, offsetY = -106, offsetWidth = 547, offsetHeight = 847}, -- 4: Head rips_OneLayer instance 10003
+		{x = 787, y = 5, width = 397, height = 576, offsetX = -60, offsetY = -112, offsetWidth = 547, offsetHeight = 847}, -- 5: Head rips_OneLayer instance 10004
+		{x = 787, y = 5, width = 397, height = 576, offsetX = -60, offsetY = -112, offsetWidth = 547, offsetHeight = 847}, -- 6: Head rips_OneLayer instance 10005
+		{x = 1184, y = 5, width = 383, height = 585, offsetX = -76, offsetY = -112, offsetWidth = 547, offsetHeight = 847}, -- 7: Head rips_OneLayer instance 10006
+		{x = 1567, y = 5, width = 354, height = 589, offsetX = -128, offsetY = -123, offsetWidth = 547, offsetHeight = 847}, -- 8: Head rips_OneLayer instance 10007
+		{x = 1567, y = 5, width = 354, height = 589, offsetX = -128, offsetY = -123, offsetWidth = 547, offsetHeight = 847}, -- 9: Head rips_OneLayer instance 10008
+		{x = 1921, y = 5, width = 354, height = 617, offsetX = -114, offsetY = -108, offsetWidth = 547, offsetHeight = 847}, -- 10: Head rips_OneLayer instance 10009
+		{x = 1921, y = 5, width = 354, height = 617, offsetX = -114, offsetY = -108, offsetWidth = 547, offsetHeight = 847}, -- 11: Head rips_OneLayer instance 10010
+		{x = 2275, y = 5, width = 355, height = 635, offsetX = -104, offsetY = -108, offsetWidth = 547, offsetHeight = 847}, -- 12: Head rips_OneLayer instance 10011
+		{x = 2630, y = 5, width = 358, height = 631, offsetX = -101, offsetY = -110, offsetWidth = 547, offsetHeight = 847}, -- 13: Head rips_OneLayer instance 10012
+		{x = 2988, y = 5, width = 357, height = 630, offsetX = -102, offsetY = -111, offsetWidth = 547, offsetHeight = 847}, -- 14: Head rips_OneLayer instance 10013
+		{x = 3345, y = 5, width = 357, height = 630, offsetX = -102, offsetY = -111, offsetWidth = 547, offsetHeight = 847}, -- 15: Head rips_OneLayer instance 10014
+		{x = 3702, y = 5, width = 355, height = 632, offsetX = -103, offsetY = -110, offsetWidth = 547, offsetHeight = 847}, -- 16: Head rips_OneLayer instance 10015
+		{x = 5, y = 640, width = 355, height = 632, offsetX = -103, offsetY = -110, offsetWidth = 547, offsetHeight = 847}, -- 17: Head rips_OneLayer instance 10016
+		{x = 360, y = 640, width = 357, height = 632, offsetX = -103, offsetY = -110, offsetWidth = 547, offsetHeight = 847}, -- 18: Head rips_OneLayer instance 10017
+		{x = 360, y = 640, width = 357, height = 632, offsetX = -103, offsetY = -110, offsetWidth = 547, offsetHeight = 847}, -- 19: Head rips_OneLayer instance 10018
+		{x = 717, y = 640, width = 383, height = 653, offsetX = -77, offsetY = -91, offsetWidth = 547, offsetHeight = 847}, -- 20: Head rips_OneLayer instance 10019
+		{x = 717, y = 640, width = 383, height = 653, offsetX = -77, offsetY = -91, offsetWidth = 547, offsetHeight = 847}, -- 21: Head rips_OneLayer instance 10020
+		{x = 1100, y = 640, width = 390, height = 671, offsetX = -66, offsetY = -95, offsetWidth = 547, offsetHeight = 847}, -- 22: Head rips_OneLayer instance 10021
+		{x = 1100, y = 640, width = 390, height = 671, offsetX = -66, offsetY = -95, offsetWidth = 547, offsetHeight = 847}, -- 23: Head rips_OneLayer instance 10022
+		{x = 1490, y = 640, width = 399, height = 699, offsetX = -46, offsetY = -91, offsetWidth = 547, offsetHeight = 847}, -- 24: Head rips_OneLayer instance 10023
+		{x = 1490, y = 640, width = 399, height = 699, offsetX = -46, offsetY = -91, offsetWidth = 547, offsetHeight = 847}, -- 25: Head rips_OneLayer instance 10024
+		{x = 1889, y = 640, width = 421, height = 679, offsetX = -13, offsetY = -116, offsetWidth = 547, offsetHeight = 847}, -- 26: Head rips_OneLayer instance 10025
+		{x = 2310, y = 640, width = 445, height = 673, offsetX = 0, offsetY = -120, offsetWidth = 547, offsetHeight = 847}, -- 27: Head rips_OneLayer instance 10026
+		{x = 2310, y = 640, width = 445, height = 673, offsetX = 0, offsetY = -120, offsetWidth = 547, offsetHeight = 847}, -- 28: Head rips_OneLayer instance 10027
+		{x = 2755, y = 640, width = 415, height = 706, offsetX = -22, offsetY = -102, offsetWidth = 547, offsetHeight = 847}, -- 29: Head rips_OneLayer instance 10028
+		{x = 3170, y = 640, width = 415, height = 706, offsetX = -22, offsetY = -102, offsetWidth = 547, offsetHeight = 847}, -- 30: Head rips_OneLayer instance 10029
+		{x = 3585, y = 640, width = 416, height = 701, offsetX = -22, offsetY = -104, offsetWidth = 547, offsetHeight = 847}, -- 31: Head rips_OneLayer instance 10030
+		{x = 5, y = 1346, width = 416, height = 701, offsetX = -22, offsetY = -104, offsetWidth = 547, offsetHeight = 847}, -- 32: Head rips_OneLayer instance 10031
+		{x = 421, y = 1346, width = 416, height = 709, offsetX = -25, offsetY = -104, offsetWidth = 547, offsetHeight = 847}, -- 33: Head rips_OneLayer instance 10032
+		{x = 837, y = 1346, width = 416, height = 709, offsetX = -25, offsetY = -104, offsetWidth = 547, offsetHeight = 847}, -- 34: Head rips_OneLayer instance 10033
+		{x = 1253, y = 1346, width = 416, height = 699, offsetX = -24, offsetY = -102, offsetWidth = 547, offsetHeight = 847}, -- 35: Head rips_OneLayer instance 10034
+		{x = 1253, y = 1346, width = 416, height = 699, offsetX = -24, offsetY = -102, offsetWidth = 547, offsetHeight = 847}, -- 36: Head rips_OneLayer instance 10035
+		{x = 1669, y = 1346, width = 422, height = 699, offsetX = -18, offsetY = -106, offsetWidth = 547, offsetHeight = 847}, -- 37: Head rips_OneLayer instance 10036
+		{x = 2091, y = 1346, width = 421, height = 694, offsetX = -17, offsetY = -120, offsetWidth = 547, offsetHeight = 847}, -- 38: Head rips_OneLayer instance 10037
+		{x = 2512, y = 1346, width = 437, height = 680, offsetX = -17, offsetY = -120, offsetWidth = 547, offsetHeight = 847}, -- 39: Head rips_OneLayer instance 10038
+		{x = 2949, y = 1346, width = 464, height = 786, offsetX = -21, offsetY = 0, offsetWidth = 547, offsetHeight = 847}, -- 40: Head rips_OneLayer instance 10039
+		{x = 2949, y = 1346, width = 464, height = 786, offsetX = -21, offsetY = 0, offsetWidth = 547, offsetHeight = 847}, -- 41: Head rips_OneLayer instance 10040
+		{x = 3413, y = 1346, width = 422, height = 732, offsetX = -32, offsetY = -88, offsetWidth = 547, offsetHeight = 847}, -- 42: Head rips_OneLayer instance 10041
+		{x = 3413, y = 1346, width = 422, height = 732, offsetX = -32, offsetY = -88, offsetWidth = 547, offsetHeight = 847}, -- 43: Head rips_OneLayer instance 10042
+		{x = 5, y = 2132, width = 398, height = 742, offsetX = -94, offsetY = -74, offsetWidth = 547, offsetHeight = 847}, -- 44: Head rips_OneLayer instance 10043
+		{x = 403, y = 2132, width = 410, height = 786, offsetX = -130, offsetY = -40, offsetWidth = 547, offsetHeight = 847}, -- 45: Head rips_OneLayer instance 10044
+		{x = 813, y = 2132, width = 453, height = 810, offsetX = -94, offsetY = -37, offsetWidth = 547, offsetHeight = 847}, -- 46: Head rips_OneLayer instance 10045
+		{x = 1266, y = 2132, width = 403, height = 768, offsetX = -110, offsetY = -39, offsetWidth = 547, offsetHeight = 847}, -- 47: Head rips_OneLayer instance 10046
+		{x = 1669, y = 2132, width = 412, height = 732, offsetX = -113, offsetY = -52, offsetWidth = 547, offsetHeight = 847}, -- 48: Head rips_OneLayer instance 10047
+		{x = 2081, y = 2132, width = 396, height = 514, offsetX = -115, offsetY = -77, offsetWidth = 547, offsetHeight = 847}, -- 49: Head rips_OneLayer instance 10048
+		{x = 2081, y = 2132, width = 396, height = 514, offsetX = -115, offsetY = -77, offsetWidth = 547, offsetHeight = 847}, -- 50: Head rips_OneLayer instance 10049
+		{x = 2477, y = 2132, width = 419, height = 530, offsetX = -69, offsetY = -103, offsetWidth = 547, offsetHeight = 847}, -- 51: Head rips_OneLayer instance 10050
+		{x = 2477, y = 2132, width = 419, height = 530, offsetX = -69, offsetY = -103, offsetWidth = 547, offsetHeight = 847}, -- 52: Head rips_OneLayer instance 10051
+		{x = 2896, y = 2132, width = 435, height = 521, offsetX = -55, offsetY = -123, offsetWidth = 547, offsetHeight = 847}, -- 53: Head rips_OneLayer instance 10052
+		{x = 2896, y = 2132, width = 435, height = 521, offsetX = -55, offsetY = -123, offsetWidth = 547, offsetHeight = 847}, -- 54: Head rips_OneLayer instance 10053
+		{x = 3331, y = 2132, width = 448, height = 512, offsetX = -47, offsetY = -132, offsetWidth = 547, offsetHeight = 847}, -- 55: Head rips_OneLayer instance 10054
+		{x = 3331, y = 2132, width = 448, height = 512, offsetX = -47, offsetY = -132, offsetWidth = 547, offsetHeight = 847}, -- 56: Head rips_OneLayer instance 10055
+		{x = 5, y = 2942, width = 460, height = 506, offsetX = -37, offsetY = -143, offsetWidth = 547, offsetHeight = 847}, -- 57: Head rips_OneLayer instance 10056
+		{x = 5, y = 2942, width = 460, height = 506, offsetX = -37, offsetY = -143, offsetWidth = 547, offsetHeight = 847}, -- 58: Head rips_OneLayer instance 10057
+		{x = 465, y = 2942, width = 470, height = 489, offsetX = -29, offsetY = -160, offsetWidth = 547, offsetHeight = 847}, -- 59: Head rips_OneLayer instance 10058
+		{x = 465, y = 2942, width = 470, height = 489, offsetX = -29, offsetY = -160, offsetWidth = 547, offsetHeight = 847}, -- 60: Head rips_OneLayer instance 10059
+		{x = 935, y = 2942, width = 469, height = 475, offsetX = -31, offsetY = -179, offsetWidth = 547, offsetHeight = 847}, -- 61: Head rips_OneLayer instance 10060
+		{x = 935, y = 2942, width = 469, height = 475, offsetX = -31, offsetY = -179, offsetWidth = 547, offsetHeight = 847}, -- 62: Head rips_OneLayer instance 10061
+		{x = 1404, y = 2942, width = 461, height = 467, offsetX = -43, offsetY = -186, offsetWidth = 547, offsetHeight = 847}, -- 63: Head rips_OneLayer instance 10062
+		{x = 1404, y = 2942, width = 461, height = 467, offsetX = -43, offsetY = -186, offsetWidth = 547, offsetHeight = 847}, -- 64: Head rips_OneLayer instance 10063
+		{x = 1865, y = 2942, width = 483, height = 481, offsetX = -26, offsetY = -172, offsetWidth = 547, offsetHeight = 847}, -- 65: Head rips_OneLayer instance 10064
+		{x = 1865, y = 2942, width = 483, height = 481, offsetX = -26, offsetY = -172, offsetWidth = 547, offsetHeight = 847}, -- 66: Head rips_OneLayer instance 10065
+		{x = 2348, y = 2942, width = 484, height = 491, offsetX = -27, offsetY = -158, offsetWidth = 547, offsetHeight = 847}, -- 67: Head rips_OneLayer instance 10066
+		{x = 2348, y = 2942, width = 484, height = 491, offsetX = -27, offsetY = -158, offsetWidth = 547, offsetHeight = 847}, -- 68: Head rips_OneLayer instance 10067
+		{x = 2832, y = 2942, width = 487, height = 493, offsetX = -27, offsetY = -154, offsetWidth = 547, offsetHeight = 847}, -- 69: Head rips_OneLayer instance 10068
+		{x = 2832, y = 2942, width = 487, height = 493, offsetX = -27, offsetY = -154, offsetWidth = 547, offsetHeight = 847} -- 70: Head rips_OneLayer instance 10069
+	},
+	{
+		["anim"] = {start = 1, stop = 70, speed = 24, offsetX = 0, offsetY = 0},
+	},
+	"anim", -- set to default animation
+	false -- If the sprite repeats
+)
---
+++ src/love/sprites/monochrome/GOLD_NO_MORE.lua
@@ -0,0 +1,72 @@
+return graphics.newSprite(
+	love.graphics.newImage(graphics.imagePath("monochrome/GOLD_NO_MORE")), -- Do not add the .png extension
+	{
+		{x = 5, y = 5, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 1: No More instance 10000
+		{x = 5, y = 5, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 2: No More instance 10001
+		{x = 393, y = 5, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 3: No More instance 10002
+		{x = 393, y = 5, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 4: No More instance 10003
+		{x = 781, y = 5, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 5: No More instance 10004
+		{x = 781, y = 5, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 6: No More instance 10005
+		{x = 1169, y = 5, width = 386, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 388, offsetHeight = 578}, -- 7: No More instance 10006
+		{x = 1169, y = 5, width = 386, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 388, offsetHeight = 578}, -- 8: No More instance 10007
+		{x = 1555, y = 5, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 9: No More instance 10008
+		{x = 1555, y = 5, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 10: No More instance 10009
+		{x = 1943, y = 5, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 11: No More instance 10010
+		{x = 1943, y = 5, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 12: No More instance 10011
+		{x = 2331, y = 5, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 13: No More instance 10012
+		{x = 2331, y = 5, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 14: No More instance 10013
+		{x = 2719, y = 5, width = 386, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 388, offsetHeight = 578}, -- 15: No More instance 10014
+		{x = 3105, y = 5, width = 386, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 388, offsetHeight = 578}, -- 16: No More instance 10015
+		{x = 3491, y = 5, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 17: No More instance 10016
+		{x = 3491, y = 5, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 18: No More instance 10017
+		{x = 5, y = 583, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 19: No More instance 10018
+		{x = 5, y = 583, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 20: No More instance 10019
+		{x = 393, y = 583, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 21: No More instance 10020
+		{x = 393, y = 583, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 22: No More instance 10021
+		{x = 781, y = 583, width = 386, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 388, offsetHeight = 578}, -- 23: No More instance 10022
+		{x = 781, y = 583, width = 386, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 388, offsetHeight = 578}, -- 24: No More instance 10023
+		{x = 1167, y = 583, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 25: No More instance 10024
+		{x = 1167, y = 583, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 26: No More instance 10025
+		{x = 1555, y = 583, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 27: No More instance 10026
+		{x = 1555, y = 583, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 28: No More instance 10027
+		{x = 1943, y = 583, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 29: No More instance 10028
+		{x = 1943, y = 583, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 30: No More instance 10029
+		{x = 2331, y = 583, width = 386, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 388, offsetHeight = 578}, -- 31: No More instance 10030
+		{x = 2331, y = 583, width = 386, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 388, offsetHeight = 578}, -- 32: No More instance 10031
+		{x = 2717, y = 583, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 33: No More instance 10032
+		{x = 2717, y = 583, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 34: No More instance 10033
+		{x = 3105, y = 583, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 35: No More instance 10034
+		{x = 3105, y = 583, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 36: No More instance 10035
+		{x = 3493, y = 583, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 37: No More instance 10036
+		{x = 3493, y = 583, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 38: No More instance 10037
+		{x = 5, y = 1161, width = 386, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 388, offsetHeight = 578}, -- 39: No More instance 10038
+		{x = 5, y = 1161, width = 386, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 388, offsetHeight = 578}, -- 40: No More instance 10039
+		{x = 391, y = 1161, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 41: No More instance 10040
+		{x = 391, y = 1161, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 42: No More instance 10041
+		{x = 779, y = 1161, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 43: No More instance 10042
+		{x = 779, y = 1161, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 44: No More instance 10043
+		{x = 1167, y = 1161, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 45: No More instance 10044
+		{x = 1167, y = 1161, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 46: No More instance 10045
+		{x = 1555, y = 1161, width = 386, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 388, offsetHeight = 578}, -- 47: No More instance 10046
+		{x = 1555, y = 1161, width = 386, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 388, offsetHeight = 578}, -- 48: No More instance 10047
+		{x = 1941, y = 1161, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 49: No More instance 10048
+		{x = 1941, y = 1161, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 50: No More instance 10049
+		{x = 2329, y = 1161, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 51: No More instance 10050
+		{x = 2329, y = 1161, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 52: No More instance 10051
+		{x = 2717, y = 1161, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 53: No More instance 10052
+		{x = 2717, y = 1161, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 54: No More instance 10053
+		{x = 1555, y = 1161, width = 386, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 388, offsetHeight = 578}, -- 55: No More instance 10054
+		{x = 1555, y = 1161, width = 386, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 388, offsetHeight = 578}, -- 56: No More instance 10055
+		{x = 1941, y = 1161, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 57: No More instance 10056
+		{x = 1941, y = 1161, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 58: No More instance 10057
+		{x = 3105, y = 1161, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 59: No More instance 10058
+		{x = 3105, y = 1161, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 60: No More instance 10059
+		{x = 3493, y = 1161, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 61: No More instance 10060
+		{x = 3493, y = 1161, width = 388, height = 578, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0} -- 62: No More instance 10061
+	},
+	{
+		["anim"] = {start = 1, stop = 62, speed = 24, offsetX = 0, offsetY = 0},
+	},
+	"anim", -- set to default animation
+	false -- If the sprite repeats
+)
