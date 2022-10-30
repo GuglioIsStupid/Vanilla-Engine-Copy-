cced32b78fc19b34f8285b60eb72c586ef50fbad
clothing-hanger <kelbymixon1@gmail.com>
2022-10-29 23:59:16 -0500

    i came on a printed out image of furry robotnik

---
+++ src/love/sprites/frostbite/Cold_Gold.lua
@@ -0,0 +1,138 @@
+return graphics.newSprite(
+	love.graphics.newImage(graphics.imagePath("frostbite/Cold_Gold")), -- Do not add the .png extension
+	{
+		{x = 0, y = 0, width = 455, height = 504, offsetX = -24, offsetY = -24, offsetWidth = 498, offsetHeight = 528}, -- 1: GOLD DOWN POSE0000
+		{x = 0, y = 0, width = 455, height = 504, offsetX = -24, offsetY = -24, offsetWidth = 498, offsetHeight = 528}, -- 2: GOLD DOWN POSE0001
+		{x = 505, y = 0, width = 445, height = 502, offsetX = -19, offsetY = -26, offsetWidth = 498, offsetHeight = 528}, -- 3: GOLD DOWN POSE0002
+		{x = 505, y = 0, width = 445, height = 502, offsetX = -19, offsetY = -26, offsetWidth = 498, offsetHeight = 528}, -- 4: GOLD DOWN POSE0003
+		{x = 1000, y = 0, width = 452, height = 508, offsetX = -6, offsetY = -13, offsetWidth = 498, offsetHeight = 528}, -- 5: GOLD DOWN POSE0004
+		{x = 1000, y = 0, width = 452, height = 508, offsetX = -6, offsetY = -13, offsetWidth = 498, offsetHeight = 528}, -- 6: GOLD DOWN POSE0005
+		{x = 1502, y = 0, width = 498, height = 515, offsetX = 0, offsetY = -3, offsetWidth = 498, offsetHeight = 528}, -- 7: GOLD DOWN POSE0006
+		{x = 1502, y = 0, width = 498, height = 515, offsetX = 0, offsetY = -3, offsetWidth = 498, offsetHeight = 528}, -- 8: GOLD DOWN POSE0007
+		{x = 2050, y = 0, width = 419, height = 516, offsetX = 0, offsetY = 0, offsetWidth = 498, offsetHeight = 528}, -- 9: GOLD DOWN POSE0008
+		{x = 2050, y = 0, width = 419, height = 516, offsetX = 0, offsetY = 0, offsetWidth = 498, offsetHeight = 528}, -- 10: GOLD DOWN POSE0009
+		{x = 2519, y = 0, width = 419, height = 516, offsetX = 0, offsetY = 0, offsetWidth = 498, offsetHeight = 528}, -- 11: GOLD DOWN POSE0010
+		{x = 2519, y = 0, width = 419, height = 516, offsetX = 0, offsetY = 0, offsetWidth = 498, offsetHeight = 528}, -- 12: GOLD DOWN POSE0011
+		{x = 2519, y = 0, width = 419, height = 516, offsetX = 0, offsetY = 0, offsetWidth = 498, offsetHeight = 528}, -- 13: GOLD DOWN POSE0012
+		{x = 2519, y = 0, width = 419, height = 516, offsetX = 0, offsetY = 0, offsetWidth = 498, offsetHeight = 528}, -- 14: GOLD DOWN POSE0013
+		{x = 2519, y = 0, width = 419, height = 516, offsetX = 0, offsetY = 0, offsetWidth = 498, offsetHeight = 528}, -- 15: GOLD DOWN POSE0014
+
+		{x = 2988, y = 0, width = 442, height = 498, offsetX = 0, offsetY = 0, offsetWidth = 455, offsetHeight = 507}, -- 16: GOLD DOWN POSE MISS0000
+		{x = 3480, y = 0, width = 448, height = 494, offsetX = -3, offsetY = -9, offsetWidth = 455, offsetHeight = 507}, -- 17: GOLD DOWN POSE MISS0001
+		{x = 3480, y = 0, width = 448, height = 494, offsetX = -3, offsetY = -9, offsetWidth = 455, offsetHeight = 507}, -- 18: GOLD DOWN POSE MISS0002
+		{x = 0, y = 566, width = 451, height = 492, offsetX = -4, offsetY = -15, offsetWidth = 455, offsetHeight = 507}, -- 19: GOLD DOWN POSE MISS0003
+		{x = 0, y = 566, width = 451, height = 492, offsetX = -4, offsetY = -15, offsetWidth = 455, offsetHeight = 507}, -- 20: GOLD DOWN POSE MISS0004
+
+		{x = 501, y = 566, width = 412, height = 524, offsetX = -11, offsetY = -20, offsetWidth = 423, offsetHeight = 545}, -- 21: GOLD IDLE0000
+		{x = 963, y = 566, width = 412, height = 524, offsetX = -11, offsetY = -20, offsetWidth = 423, offsetHeight = 545}, -- 22: GOLD IDLE0001
+		{x = 1425, y = 566, width = 408, height = 524, offsetX = -13, offsetY = -21, offsetWidth = 423, offsetHeight = 545}, -- 23: GOLD IDLE0002
+		{x = 1883, y = 566, width = 403, height = 534, offsetX = -7, offsetY = -11, offsetWidth = 423, offsetHeight = 545}, -- 24: GOLD IDLE0003
+		{x = 2336, y = 566, width = 406, height = 545, offsetX = -1, offsetY = 0, offsetWidth = 423, offsetHeight = 545}, -- 25: GOLD IDLE0004
+		{x = 2792, y = 566, width = 402, height = 545, offsetX = -1, offsetY = 0, offsetWidth = 423, offsetHeight = 545}, -- 26: GOLD IDLE0005
+		{x = 3244, y = 566, width = 405, height = 545, offsetX = 0, offsetY = 0, offsetWidth = 423, offsetHeight = 545}, -- 27: GOLD IDLE0006
+		{x = 0, y = 1161, width = 405, height = 545, offsetX = 0, offsetY = 0, offsetWidth = 423, offsetHeight = 545}, -- 28: GOLD IDLE0007
+		{x = 455, y = 1161, width = 405, height = 545, offsetX = 0, offsetY = 0, offsetWidth = 423, offsetHeight = 545}, -- 29: GOLD IDLE0008
+		{x = 910, y = 1161, width = 407, height = 545, offsetX = 0, offsetY = 0, offsetWidth = 423, offsetHeight = 545}, -- 30: GOLD IDLE0009
+		{x = 910, y = 1161, width = 407, height = 545, offsetX = 0, offsetY = 0, offsetWidth = 423, offsetHeight = 545}, -- 31: GOLD IDLE0010
+		{x = 910, y = 1161, width = 407, height = 545, offsetX = 0, offsetY = 0, offsetWidth = 423, offsetHeight = 545}, -- 32: GOLD IDLE0011
+		{x = 910, y = 1161, width = 407, height = 545, offsetX = 0, offsetY = 0, offsetWidth = 423, offsetHeight = 545}, -- 33: GOLD IDLE0012
+		{x = 910, y = 1161, width = 407, height = 545, offsetX = 0, offsetY = 0, offsetWidth = 423, offsetHeight = 545}, -- 34: GOLD IDLE0013
+		{x = 910, y = 1161, width = 407, height = 545, offsetX = 0, offsetY = 0, offsetWidth = 423, offsetHeight = 545}, -- 35: GOLD IDLE0014
+
+		{x = 1367, y = 1161, width = 496, height = 539, offsetX = 0, offsetY = -11, offsetWidth = 522, offsetHeight = 552}, -- 36: GOLD LEFT POSE0000
+		{x = 1367, y = 1161, width = 496, height = 539, offsetX = 0, offsetY = -11, offsetWidth = 522, offsetHeight = 552}, -- 37: GOLD LEFT POSE0001
+		{x = 1913, y = 1161, width = 488, height = 548, offsetX = -18, offsetY = -4, offsetWidth = 522, offsetHeight = 552}, -- 38: GOLD LEFT POSE0002
+		{x = 1913, y = 1161, width = 488, height = 548, offsetX = -18, offsetY = -4, offsetWidth = 522, offsetHeight = 552}, -- 39: GOLD LEFT POSE0003
+		{x = 2451, y = 1161, width = 462, height = 550, offsetX = -54, offsetY = 0, offsetWidth = 522, offsetHeight = 552}, -- 40: GOLD LEFT POSE0004
+		{x = 2451, y = 1161, width = 462, height = 550, offsetX = -54, offsetY = 0, offsetWidth = 522, offsetHeight = 552}, -- 41: GOLD LEFT POSE0005
+		{x = 2963, y = 1161, width = 446, height = 548, offsetX = -73, offsetY = -1, offsetWidth = 522, offsetHeight = 552}, -- 42: GOLD LEFT POSE0006
+		{x = 2963, y = 1161, width = 446, height = 548, offsetX = -73, offsetY = -1, offsetWidth = 522, offsetHeight = 552}, -- 43: GOLD LEFT POSE0007
+		{x = 3459, y = 1161, width = 445, height = 547, offsetX = -77, offsetY = -2, offsetWidth = 522, offsetHeight = 552}, -- 44: GOLD LEFT POSE0008
+		{x = 3459, y = 1161, width = 445, height = 547, offsetX = -77, offsetY = -2, offsetWidth = 522, offsetHeight = 552}, -- 45: GOLD LEFT POSE0009
+		{x = 0, y = 1761, width = 445, height = 547, offsetX = -77, offsetY = -2, offsetWidth = 522, offsetHeight = 552}, -- 46: GOLD LEFT POSE0010
+		{x = 0, y = 1761, width = 445, height = 547, offsetX = -77, offsetY = -2, offsetWidth = 522, offsetHeight = 552}, -- 47: GOLD LEFT POSE0011
+		{x = 0, y = 1761, width = 445, height = 547, offsetX = -77, offsetY = -2, offsetWidth = 522, offsetHeight = 552}, -- 48: GOLD LEFT POSE0012
+		{x = 0, y = 1761, width = 445, height = 547, offsetX = -77, offsetY = -2, offsetWidth = 522, offsetHeight = 552}, -- 49: GOLD LEFT POSE0013
+		{x = 0, y = 1761, width = 445, height = 547, offsetX = -77, offsetY = -2, offsetWidth = 522, offsetHeight = 552}, -- 50: GOLD LEFT POSE0014
+
+		{x = 495, y = 1761, width = 445, height = 546, offsetX = -42, offsetY = 0, offsetWidth = 487, offsetHeight = 560}, -- 51: GOLD LEFT POSE MISS0000
+		{x = 990, y = 1761, width = 464, height = 548, offsetX = -7, offsetY = -7, offsetWidth = 487, offsetHeight = 560}, -- 52: GOLD LEFT POSE MISS0001
+		{x = 990, y = 1761, width = 464, height = 548, offsetX = -7, offsetY = -7, offsetWidth = 487, offsetHeight = 560}, -- 53: GOLD LEFT POSE MISS0002
+		{x = 1504, y = 1761, width = 471, height = 552, offsetX = 0, offsetY = -8, offsetWidth = 487, offsetHeight = 560}, -- 54: GOLD LEFT POSE MISS0003
+		{x = 1504, y = 1761, width = 471, height = 552, offsetX = 0, offsetY = -8, offsetWidth = 487, offsetHeight = 560}, -- 55: GOLD LEFT POSE MISS0004
+
+		{x = 2025, y = 1761, width = 428, height = 591, offsetX = -8, offsetY = -33, offsetWidth = 436, offsetHeight = 624}, -- 56: GOLD MISS UP0000
+		{x = 2503, y = 1761, width = 434, height = 622, offsetX = -2, offsetY = 0, offsetWidth = 436, offsetHeight = 624}, -- 57: GOLD MISS UP0001
+		{x = 2503, y = 1761, width = 434, height = 622, offsetX = -2, offsetY = 0, offsetWidth = 436, offsetHeight = 624}, -- 58: GOLD MISS UP0002
+		{x = 2987, y = 1761, width = 436, height = 614, offsetX = 0, offsetY = -8, offsetWidth = 436, offsetHeight = 624}, -- 59: GOLD MISS UP0003
+		{x = 2987, y = 1761, width = 436, height = 614, offsetX = 0, offsetY = -8, offsetWidth = 436, offsetHeight = 624}, -- 60: GOLD MISS UP0004
+
+		{x = 3473, y = 1761, width = 423, height = 530, offsetX = -5, offsetY = 0, offsetWidth = 455, offsetHeight = 530}, -- 61: GOLD RIGHT POSE0000
+		{x = 3473, y = 1761, width = 423, height = 530, offsetX = -5, offsetY = 0, offsetWidth = 455, offsetHeight = 530}, -- 62: GOLD RIGHT POSE0001
+		{x = 0, y = 2433, width = 425, height = 523, offsetX = -2, offsetY = -7, offsetWidth = 455, offsetHeight = 530}, -- 63: GOLD RIGHT POSE0002
+		{x = 0, y = 2433, width = 425, height = 523, offsetX = -2, offsetY = -7, offsetWidth = 455, offsetHeight = 530}, -- 64: GOLD RIGHT POSE0003
+		{x = 475, y = 2433, width = 433, height = 523, offsetX = -2, offsetY = -7, offsetWidth = 455, offsetHeight = 530}, -- 65: GOLD RIGHT POSE0004
+		{x = 475, y = 2433, width = 433, height = 523, offsetX = -2, offsetY = -7, offsetWidth = 455, offsetHeight = 530}, -- 66: GOLD RIGHT POSE0005
+		{x = 958, y = 2433, width = 454, height = 523, offsetX = -1, offsetY = -7, offsetWidth = 455, offsetHeight = 530}, -- 67: GOLD RIGHT POSE0006
+		{x = 958, y = 2433, width = 454, height = 523, offsetX = -1, offsetY = -7, offsetWidth = 455, offsetHeight = 530}, -- 68: GOLD RIGHT POSE0007
+		{x = 1462, y = 2433, width = 422, height = 523, offsetX = 0, offsetY = -7, offsetWidth = 455, offsetHeight = 530}, -- 69: GOLD RIGHT POSE0008
+		{x = 1462, y = 2433, width = 422, height = 523, offsetX = 0, offsetY = -7, offsetWidth = 455, offsetHeight = 530}, -- 70: GOLD RIGHT POSE0009
+		{x = 1934, y = 2433, width = 422, height = 523, offsetX = 0, offsetY = -7, offsetWidth = 455, offsetHeight = 530}, -- 71: GOLD RIGHT POSE0010
+		{x = 1934, y = 2433, width = 422, height = 523, offsetX = 0, offsetY = -7, offsetWidth = 455, offsetHeight = 530}, -- 72: GOLD RIGHT POSE0011
+		{x = 1934, y = 2433, width = 422, height = 523, offsetX = 0, offsetY = -7, offsetWidth = 455, offsetHeight = 530}, -- 73: GOLD RIGHT POSE0012
+		{x = 1934, y = 2433, width = 422, height = 523, offsetX = 0, offsetY = -7, offsetWidth = 455, offsetHeight = 530}, -- 74: GOLD RIGHT POSE0013
+		{x = 1934, y = 2433, width = 422, height = 523, offsetX = 0, offsetY = -7, offsetWidth = 455, offsetHeight = 530}, -- 75: GOLD RIGHT POSE0014
+
+		{x = 2406, y = 2433, width = 422, height = 523, offsetX = 0, offsetY = 0, offsetWidth = 425, offsetHeight = 525}, -- 76: GOLD RIGHT POSE MISS0000
+		{x = 2878, y = 2433, width = 422, height = 520, offsetX = -2, offsetY = -3, offsetWidth = 425, offsetHeight = 525}, -- 77: GOLD RIGHT POSE MISS0001
+		{x = 2878, y = 2433, width = 422, height = 520, offsetX = -2, offsetY = -3, offsetWidth = 425, offsetHeight = 525}, -- 78: GOLD RIGHT POSE MISS0002
+		{x = 3350, y = 2433, width = 422, height = 516, offsetX = -3, offsetY = -9, offsetWidth = 425, offsetHeight = 525}, -- 79: GOLD RIGHT POSE MISS0003
+		{x = 3350, y = 2433, width = 422, height = 516, offsetX = -3, offsetY = -9, offsetWidth = 425, offsetHeight = 525}, -- 80: GOLD RIGHT POSE MISS0004
+
+		{x = 0, y = 3006, width = 429, height = 592, offsetX = -8, offsetY = 0, offsetWidth = 437, offsetHeight = 592}, -- 81: GOLD UP MISS0000
+		{x = 0, y = 3006, width = 429, height = 592, offsetX = -8, offsetY = 0, offsetWidth = 437, offsetHeight = 592}, -- 82: GOLD UP MISS0001
+		{x = 479, y = 3006, width = 436, height = 586, offsetX = 0, offsetY = -5, offsetWidth = 437, offsetHeight = 592}, -- 83: GOLD UP MISS0002
+		{x = 479, y = 3006, width = 436, height = 586, offsetX = 0, offsetY = -5, offsetWidth = 437, offsetHeight = 592}, -- 84: GOLD UP MISS0003
+		{x = 479, y = 3006, width = 436, height = 586, offsetX = 0, offsetY = -5, offsetWidth = 437, offsetHeight = 592}, -- 85: GOLD UP MISS0004
+		{x = 479, y = 3006, width = 436, height = 586, offsetX = 0, offsetY = -5, offsetWidth = 437, offsetHeight = 592}, -- 86: GOLD UP MISS0005
+		{x = 479, y = 3006, width = 436, height = 586, offsetX = 0, offsetY = -5, offsetWidth = 437, offsetHeight = 592}, -- 87: GOLD UP MISS0006
+		{x = 479, y = 3006, width = 436, height = 586, offsetX = 0, offsetY = -5, offsetWidth = 437, offsetHeight = 592}, -- 88: GOLD UP MISS0007
+		{x = 479, y = 3006, width = 436, height = 586, offsetX = 0, offsetY = -5, offsetWidth = 437, offsetHeight = 592}, -- 89: GOLD UP MISS0008
+		{x = 479, y = 3006, width = 436, height = 586, offsetX = 0, offsetY = -5, offsetWidth = 437, offsetHeight = 592}, -- 90: GOLD UP MISS0009
+		{x = 479, y = 3006, width = 436, height = 586, offsetX = 0, offsetY = -5, offsetWidth = 437, offsetHeight = 592}, -- 91: GOLD UP MISS0010
+		{x = 479, y = 3006, width = 436, height = 586, offsetX = 0, offsetY = -5, offsetWidth = 437, offsetHeight = 592}, -- 92: GOLD UP MISS0011
+		{x = 479, y = 3006, width = 436, height = 586, offsetX = 0, offsetY = -5, offsetWidth = 437, offsetHeight = 592}, -- 93: GOLD UP MISS0012
+		{x = 479, y = 3006, width = 436, height = 586, offsetX = 0, offsetY = -5, offsetWidth = 437, offsetHeight = 592}, -- 94: GOLD UP MISS0013
+		{x = 479, y = 3006, width = 436, height = 586, offsetX = 0, offsetY = -5, offsetWidth = 437, offsetHeight = 592}, -- 95: GOLD UP MISS0014
+
+		{x = 965, y = 3006, width = 417, height = 624, offsetX = -14, offsetY = 0, offsetWidth = 440, offsetHeight = 624}, -- 96: GOLD UP POSE0000
+		{x = 965, y = 3006, width = 417, height = 624, offsetX = -14, offsetY = 0, offsetWidth = 440, offsetHeight = 624}, -- 97: GOLD UP POSE0001
+		{x = 1432, y = 3006, width = 435, height = 597, offsetX = -5, offsetY = -15, offsetWidth = 440, offsetHeight = 624}, -- 98: GOLD UP POSE0002
+		{x = 1432, y = 3006, width = 435, height = 597, offsetX = -5, offsetY = -15, offsetWidth = 440, offsetHeight = 624}, -- 99: GOLD UP POSE0003
+		{x = 1917, y = 3006, width = 433, height = 587, offsetX = -1, offsetY = -26, offsetWidth = 440, offsetHeight = 624}, -- 100: GOLD UP POSE0004
+		{x = 1917, y = 3006, width = 433, height = 587, offsetX = -1, offsetY = -26, offsetWidth = 440, offsetHeight = 624}, -- 101: GOLD UP POSE0005
+		{x = 2400, y = 3006, width = 434, height = 587, offsetX = 0, offsetY = -25, offsetWidth = 440, offsetHeight = 624}, -- 102: GOLD UP POSE0006
+		{x = 2400, y = 3006, width = 434, height = 587, offsetX = 0, offsetY = -25, offsetWidth = 440, offsetHeight = 624}, -- 103: GOLD UP POSE0007
+		{x = 2884, y = 3006, width = 434, height = 587, offsetX = 0, offsetY = -25, offsetWidth = 440, offsetHeight = 624}, -- 104: GOLD UP POSE0008
+		{x = 2884, y = 3006, width = 434, height = 587, offsetX = 0, offsetY = -25, offsetWidth = 440, offsetHeight = 624}, -- 105: GOLD UP POSE0009
+		{x = 3368, y = 3006, width = 434, height = 587, offsetX = 0, offsetY = -25, offsetWidth = 440, offsetHeight = 624}, -- 106: GOLD UP POSE0010
+		{x = 3368, y = 3006, width = 434, height = 587, offsetX = 0, offsetY = -25, offsetWidth = 440, offsetHeight = 624}, -- 107: GOLD UP POSE0011
+		{x = 3368, y = 3006, width = 434, height = 587, offsetX = 0, offsetY = -25, offsetWidth = 440, offsetHeight = 624}, -- 108: GOLD UP POSE0012
+		{x = 3368, y = 3006, width = 434, height = 587, offsetX = 0, offsetY = -25, offsetWidth = 440, offsetHeight = 624}, -- 109: GOLD UP POSE0013
+		{x = 3368, y = 3006, width = 434, height = 587, offsetX = 0, offsetY = -25, offsetWidth = 440, offsetHeight = 624} -- 110: GOLD UP POSE0014
+	},
+	{
+		["idle"] = {start = 21, stop = 35, speed = 24, offsetX = 0, offsetY = 0},
+		["left"] = {start = 36, stop = 50, speed = 24, offsetX = 0, offsetY = 0},
+		["down"] = {start = 1, stop = 15, speed = 24, offsetX = 0, offsetY = 0},
+		["up"] = {start = 96, stop = 110, speed = 24, offsetX = 0, offsetY = 0},
+		["right"] = {start = 61, stop = 75, speed = 24, offsetX = 0, offsetY = 0},
+		["miss left"] = {start = 51, stop = 55, speed = 24, offsetX = 0, offsetY = 0},
+		["miss down"] = {start = 16, stop = 20, speed = 24, offsetX = 0, offsetY = 0},
+		["miss right"] = {start = 76, stop = 80, speed = 24, offsetX = 0, offsetY = 0},
+		["miss up"] = {start = 81, stop = 95, speed = 24, offsetX = 0, offsetY = 0},
+		["miss up 2??"] = {start = 56, stop = 60, speed = 24, offsetX = 0, offsetY = 0}
+	},
+	"idle", -- set to default animation
+	false -- If the sprite repeats
+)
